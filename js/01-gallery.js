70dc3d5fa23576009c2b199343efc22295c8d09e
Andrii-Val <andrii.val1006@gmail.com>
2023-06-20 13:03:36 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `).join('');
+}
+
+const openModal = (event) => {
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  };
+  
+  const largeImageUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600" />
+  `);
+
+  instance.show();
+}
+
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+gallery.addEventListener('click', openModal);
 
 console.log(galleryItems);
